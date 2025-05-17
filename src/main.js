f988657bcf1e1c6229eac4fffc2126bfd2a44f3d
Ajeeth Adithya <ajeeth5999@gmail.com>
2025-05-17 10:54:29 -0700

    Make use of the todoist API to get the projects and tasks list

--- src/main.js
+++ src/main.js
@@ -1,13 +1,54 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
+const {TodoistApi} = require('@doist/todoist-api-typescript');
+const api = new TodoistApi(process.env.TODOIST_API_KEY);
+
+async function getProjects() {
+        try {
+                const projects = await api.getProjects();
+                return projects;
+        }
+        catch(error){
+                console.error('error', error);
+        }
+}
+
+async function getTasks(project_id) {
+
+	try {
+		const tasks = await api.getTasks({project_id});
+		return tasks
+	}
+	catch {
+		console.error('error', error);
+	}
+}
 
 (async function run() {
-	console.log('Running my daily report..')	
 
+	console.log('Running my daily report..\n')	
+
+	const projects = await getProjects();
+	//console.log(projects);
+
+	for (let i = 1; i < projects.results.length; i++) {
+		console.log(projects.results[i].name);
+		const tasks = await getTasks(projects.results[i].id);
+		//console.log(tasks);
+		for(let j = 0; j < tasks.results.length; j++) {
+			console.log("\t", tasks.results[j].content);
+		}
+		console.log("\n");
+	}
+
+	//const tasks = await getTasks(projects.results[1].id);
+        //console.log(tasks);
+
+	/* Email code */
 	const transporter = nodemailer.createTransport({
   		host: "smtp.gmail.com",
   		port: 465,
-  		secure: true, // true for 465, false for other ports
+  		secure: true,
   		auth: {
     			user: process.env.MAIL_USER_EMAIL,
     			pass: process.env.MAIL_USER_PASSWORD,
@@ -17,12 +58,12 @@ const nodemailer = require('nodemailer');
 	let info = await transporter.sendMail({
 		from: process.env.MAIL_FROM,
 		to: process.env.MAIL_TO,
-		subject: "To-Do List",
+		subject: "Todoist",
 		text: `
-			Hello world!
+			Todoist!
 		`,
 		html: `
-			<h1>Hello world!</h1>
+			<ht>Welcome</h1>
 		`,
 	});
 
